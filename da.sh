07ec31969a50738581c164a5b5c993b65cbf70cb
Mário Carranca <mariokarras@gmail.com>
2014-07-12 09:19:46 -0400

    Actually use the modified time value from the config file

--- da.sh
+++ da.sh
@@ -1,19 +1,15 @@
 #!/usr/local/bin/bash
-#TODO: Actually read the server-side modified time from the variable in the config file (and get rid of MODIFIED_TIMES)
 #TODO: Get the modified time from the CURL call and save it to the config file (http://stackoverflow.com/questions/2464760/modify-config-file-using-bash-script)
 
 FILES=( 'login'  )
 source da_update.cfg
 
-declare -A MODIFIED_TIMES
-
-MODIFIED_TIMES['login']="19700101 00:00:00 -0000"
-
 for FILENAME in "${FILES[@]}"; 
 do 
+	MODIFIED_TIME=${!FILENAME}
 	FILENAME=$FILENAME.rar
-	echo "Fetching $FILENAME"
-	curl -s -o $FILENAME -z "${MODIFIED_TIMES[$FILENAME]}" http://www.darkagerp.com/release/$FILENAME
+	echo "Fetching $FILENAME, last fetched $MODIFIED_TIME"
+	curl -s -o $FILENAME -z "$MODIFIED_TIME" http://www.darkagerp.com/release/$FILENAME
 
 	if [ -e $FILENAME ]
 	then
