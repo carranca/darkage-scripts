49fc8c0aca470e7b75631120af32e4edacece3f7
Mário Carranca <mariokarras@gmail.com>
2014-07-12 00:43:52 -0400

    Add support for a separate file where things are stored

--- da.sh
+++ da.sh
@@ -1,17 +1,20 @@
 #!/usr/local/bin/bash
 
+FILES=( 'login'  )
+source da_update.cfg
+
 declare -A MODIFIED_TIMES
 
-MODIFIED_TIMES['login.rar']="19700101 00:00:00 -0000"
+MODIFIED_TIMES['login']="19700101 00:00:00 -0000"
 
-for FILENAME in "${!MODIFIED_TIMES[@]}"; 
+for FILENAME in "${FILES[@]}"; 
 do 
+	FILENAME=$FILENAME.rar
 	echo "Fetching $FILENAME"
 	curl -s -o $FILENAME -z "${MODIFIED_TIMES[$FILENAME]}" http://www.darkagerp.com/release/$FILENAME
 
 	if [ -e $FILENAME ]
 	then
-		echo 'Huzzah!'
 		if ! hash unrar 2>/dev/null; then
 			echo 'Installing unrar dependency'
 			brew install unrar
